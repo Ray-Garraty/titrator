8f05b361b66a7613e806637b093e3116fe33c7f7
Ray-Garraty <vl.besed@gmail.com>
2025-09-26 16:57:43 +0300

    Added temperature ADC reading and basic NTC calibration

--- src/hardware/adc.ts
+++ src/hardware/adc.ts
@@ -2,28 +2,49 @@ import ADS1115 from "ads1115";
 import chalk from "chalk";
 
 const i2cBus = 1;
-const adcChannelNum = 0;
+const mvChannelNum = 0;
+const tempChannelNum = 1;
 const adcAddress = 0x48;
 const adsModule = await ADS1115.open(i2cBus, adcAddress, "i2c-bus");
 adsModule.gain = 1;
 
+const A = 1.451521922 * 10 ** -3;
+const B = 2.411232075 * 10 ** -4;
+const C = 0.8942207997 * 10 ** -7;
+
 const pollPeriod = 100;
 let mV = 0;
+let temperature = 0;
 
-const measureMv = async (): Promise<number> => {
-  const rawReading: number = await adsModule.measure(`${adcChannelNum}+GND`);
-  mV = Math.round(rawReading * 0.1253 - 1508.25);
+const measureMv = async () => {
+  const rawMvReading: number = await adsModule.measure(`${mvChannelNum}+GND`);
+  mV = Math.round(rawMvReading * 0.1253 - 1507.25);
+  const rawTempReading: number = await adsModule.measure(
+    `${tempChannelNum}+GND`,
+  );
+
+  const resistance = Math.round(-0.675 * rawTempReading + 8797);
+
+  temperature = Number(
+    (
+      1 /
+        (A +
+          B * Math.log(resistance) +
+          C * Math.abs(Math.log(resistance)) ** 3) -
+      273
+    ).toFixed(1),
+  );
+
   /* console.log(
     rawReading,
     chalk.yellowBright("ADC,"),
     chalk.greenBright(mV),
     chalk.greenBright("mV"),
   ); */
-  return mV;
 };
 
 setInterval(measureMv, pollPeriod);
 
 process.on("message", () => {
-  process.send(mV);
+  process.send([mV, temperature]);
 });
--- src/index.ts
+++ src/index.ts
@@ -9,21 +9,23 @@ const dirName = path.dirname(fileName);
 
 const adcPollPeriod = 1000;
 let electrodeMv = 0;
+let temperature = 0;
 
 // ADC Electrode mV polling setup
 const adcProcess = fork(`${dirName}/hardware/adc.ts`);
 
-adcProcess.on("message", (data) => {
-  electrodeMv = Number(data);
-  console.log(electrodeMv, chalk.yellow("mV"));
+adcProcess.on("message", ([mV, temp]) => {
+  electrodeMv = mV;
+  temperature = temp;
+  console.log(electrodeMv, chalk.yellow("mV"), ",", chalk.blue(temperature));
 });
 
 adcProcess.on("close", (code) => {
   console.log(chalk.redBright(`ADC process exited with code ${code}`));
 });
 
-const pollAdc = async () => {
-  await adcProcess.send("Give me the electrode mV");
+const pollAdc = () => {
+  adcProcess.send("Give me the electrode mV");
 };
 
 setInterval(pollAdc, adcPollPeriod);
